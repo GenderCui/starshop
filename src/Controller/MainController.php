60a1ffefa406749840aa77dfc118ec020d2758f9
GenderCui <vn.navarretegarcia@gmail.com>
2025-02-07 17:54:25 +0100

    modificado el main controller para enlazar con la plantilla de twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,18 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class MainController extends AbstractController
+class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): JsonResponse
+    #[Route('/')]
+    public function homepage(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
+        $starshipCount = 457;
 
-            'path' => 'src/Controller/MainController.php',
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
         ]);
     }
 }
