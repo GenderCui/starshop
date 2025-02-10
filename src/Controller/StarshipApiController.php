51e3b3cf1c29f0b66a3c6bcbb18f8a76fab10f7b
GenderCui <vn.navarretegarcia@gmail.com>
2025-02-10 15:52:03 +0100

    instalado asset mapper

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -7,13 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships/{id<\d+>}', methods:['GET'])]
-    public function getCollection(StarshipRepository $repository, int $id): Response
+    #[Route('')]
+    public function getCollection(StarshipRepository $repository): Response
     {
         $starships = $repository->findAll();
 
         return $this->json($starships);
+    }
+    #[Route('{id<\d+>}', methods:['GET','POST'])]
+    public function get(StarshipRepository $repository, int $id = 0) {
+
     }
 }
